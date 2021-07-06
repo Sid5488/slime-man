d70ded2bceeaf2982dd77be3b8c551784a87c00c
Guilherme Sousa <guilhermenoobsaibot@gmail.com>
2021-07-06 19:18:38 -0300

    Elements manual in screen

--- src/config/window.py
+++ src/config/window.py
@@ -1,5 +1,7 @@
 import pygame
 
+from scripts.player.player import Player
+
 
 class Window:
     WIDTH = 840
@@ -8,13 +10,16 @@ class Window:
     display = pygame.display.set_mode([WIDTH, HEIGHT])
     draw_group = pygame.sprite.Group()
 
+    player: Player
+
+    def __init__(self):
+        self.player = Player()
+
     def draw(self):
         pygame.display.set_caption('Meu Super Jogo')
         self.display.fill([46, 46, 46])
-        self.render_elements()
+        self.group_of_draw()
 
-    def render_elements(self, elements=[]):
-        for element in elements:
-            self.draw_group.add(element)
-
+    def group_of_draw(self):
+        self.draw_group.add(self.player.slime_man)
         self.draw_group.draw(self.display)
